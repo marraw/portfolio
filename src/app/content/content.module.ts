787d63c1620090d8d48392e8d0e80650e4b1f8a3
Marek Rawdanowicz <marek.rawdanowicz@outlook.com>
2020-12-11 13:06:06 +0100

    add projects and contact template

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -3,10 +3,24 @@ import { CommonModule } from '@angular/common';
 import { StartComponent } from './start/start.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [StartComponent, AboutComponent, SkillsComponent],
+  declarations: [
+    StartComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+  ],
   imports: [CommonModule],
-  exports: [StartComponent, AboutComponent, SkillsComponent],
+  exports: [
+    StartComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+  ],
 })
 export class ContentModule {}
