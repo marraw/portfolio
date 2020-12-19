01078740d52958312a6ee2d13e1527e0bd2eeccd
Marek Rawdanowicz <marek.rawdanowicz@outlook.com>
2020-12-19 19:17:20 +0100

    fix firefox outline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,8 @@
-import {
-  Component,
-  ElementRef,
-  HostListener,
-  Renderer2,
-  ViewChild,
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  @ViewChild('start') start!: ElementRef;
-
-  constructor(private renderer: Renderer2) {}
-
-  @HostListener('window:scroll')
-  hideAnimation(): void {
-    if (window.pageYOffset > window.innerHeight) {
-      this.renderer.setStyle(this.start.nativeElement, 'visibility', 'hidden');
-    } else {
-      this.renderer.setStyle(this.start.nativeElement, 'visibility', 'visible');
-    }
-  }
-}
+export class AppComponent {}
